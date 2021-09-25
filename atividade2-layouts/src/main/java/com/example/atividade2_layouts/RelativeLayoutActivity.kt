9a729dacdb511ebacb30fe50660052a00cafb668
Marcos André <marcosandre@cc.ci.ufpb.br>
2021-09-25 10:53:40 -0300

    Implementar primeiro layout da segunda atividade

---
+++ atividade2-layouts/src/main/java/com/example/atividade2_layouts/RelativeLayoutActivity.kt
@@ -0,0 +1,23 @@
+package com.example.atividade2_layouts
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.atividade2_layouts.databinding.ActivityRelativeLayoutBinding
+
+private lateinit var binding: ActivityRelativeLayoutBinding
+
+class RelativeLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relative_layout)
+
+        binding = ActivityRelativeLayoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.nextButton.setOnClickListener {
+            val intent = Intent(this, GridLayoutActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
