56f104c4f3ecfc8e97321a99a91857081e8fd438
Marcos André <marcosandre@cc.ci.ufpb.br>
2021-09-28 21:31:23 -0300

    (Item 4 do Slide) Refazer exercício 3 com ConstraintLayout

---
+++ atividade2-layouts/src/main/java/com/example/atividade2_layouts/Exercicio3ConstraintLayoutActivity.kt
@@ -0,0 +1,11 @@
+package com.example.atividade2_layouts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Exercicio3ConstrainLayoutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exercicio3_constraint_layout)
+    }
+}
--- atividade2-layouts/src/main/java/com/example/atividade2_layouts/TableLayoutActivity.kt
+++ atividade2-layouts/src/main/java/com/example/atividade2_layouts/TableLayoutActivity.kt
@@ -1,7 +1,9 @@
 package com.example.atividade2_layouts
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.atividade2_layouts.databinding.ActivityRelativeLayoutBinding
 import com.example.atividade2_layouts.databinding.ActivityTableLayoutBinding
 
 private lateinit var binding: ActivityTableLayoutBinding
@@ -10,6 +12,14 @@ class TableLayoutActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_table_layout)
+
+        binding = ActivityTableLayoutBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.nextButton.setOnClickListener {
+            val intent = Intent(this, Exercicio3ConstrainLayoutActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 
